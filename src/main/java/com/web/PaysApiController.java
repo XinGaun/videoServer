5a617e4177ded5d1e8fa3a05a4c32210e488960b
guanxin <m17621995911@163.com>
2018-05-16 12:01:14 +0800

    ss

---
+++ src/main/java/com/web/PaysApiController.java
@@ -0,0 +1,65 @@
+package com.web;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSON;
+import com.entity.PaySaPi;
+import com.util.PayUtil;
+
+@Controller
+@RequestMapping("pays")
+public class PaysApiController {
+	@RequestMapping(value="/pay",produces="application/json;charset=utf-8",method=RequestMethod.POST)
+	@ResponseBody
+	public String pay(HttpServletRequest request, float price, int istype) throws UnsupportedEncodingException {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		Map<String, Object> remoteMap = new HashMap<String, Object>();
+		System.out.println(price);
+		remoteMap.put("price", price);
+		remoteMap.put("istype", istype);
+		remoteMap.put("orderid", PayUtil.getOrderIdByUUId());
+		remoteMap.put("orderuid", "911");
+		//remoteMap.put("goodsname", "测试");
+		resultMap.put("data", PayUtil.payOrder(remoteMap));
+		return  JSON.toJSONString(resultMap);
+	}
+	
+	@RequestMapping("/notifyPay")
+	public void notifyPay(HttpServletRequest request, HttpServletResponse response, PaySaPi paySaPi) throws UnsupportedEncodingException {
+		// 保证密钥一致性
+		if (PayUtil.checkPayKey(paySaPi)) {
+			System.out.println("price:"+paySaPi.getPrice());
+			System.out.println("spi_id: "+paySaPi.getPaysapi_id());
+			response.setStatus(200);
+			// TODO 做自己想做的
+		} else {
+			// TODO 该怎么做就怎么做
+			System.out.println("秘钥不一致");
+			response.setStatus(400);
+		}
+	}
+	
+	@RequestMapping("/returnPay")
+	public ModelAndView returnPay(HttpServletRequest request, HttpServletResponse response, String orderid) {
+		boolean isTrue = false;
+		ModelAndView view = null;
+		// 根据订单号查找相应的记录:根据结果跳转到不同的页面
+		if (isTrue) {
+			view = new ModelAndView("success");
+		} else {
+			view = new ModelAndView("error");
+		}
+		return view;
+	}
+}
---
+++ target/classes/com/service/serviceImpl/QQemailService.java
@@ -0,0 +1,70 @@
+package com.service.serviceImpl;
+
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.sun.mail.util.MailSSLSocketFactory;
+
+@Service
+public class QQemailService {
+	private static String userAddr="326379827@qq.com";//发送的邮箱地址
+	private static String userSecurity="cneomhjnzpzpbgee";//授权码
+	/**
+	 * 
+	 * @param email 邮箱地址
+	 * @param title	邮件标题
+	 * @param text	邮件内容
+	 * @throws MessagingException
+	 * @throws GeneralSecurityException
+	 */
+	public static String emit(String data) throws MessagingException, GeneralSecurityException {
+		@SuppressWarnings("unchecked")
+		HashMap<String,Object> map = JSON.parseObject(data,HashMap.class);
+		String title=map.get("title").toString();
+		String text =map.get("text").toString();
+		String email =map.get("email").toString();
+		
+		Properties props = new Properties();
+
+		// 开启debug调试
+		props.setProperty("mail.debug", "true");
+		// 发送服务器需要身份验证
+		props.setProperty("mail.smtp.auth", "true");
+		// 设置邮件服务器主机名
+		props.setProperty("mail.host", "smtp.qq.com");
+		// 发送邮件协议名称
+		props.setProperty("mail.transport.protocol", "smtp");
+
+		MailSSLSocketFactory sf = new MailSSLSocketFactory();
+		sf.setTrustAllHosts(true);
+		props.put("mail.smtp.ssl.enable", "true");
+		props.put("mail.smtp.ssl.socketFactory", sf);
+
+		Session session = Session.getInstance(props);
+
+		Message msg = new MimeMessage(session);
+		msg.setSubject(title);
+		msg.setText(text);
+		msg.setFrom(new InternetAddress(userAddr));
+
+		Transport transport = session.getTransport();
+		transport.connect("smtp.qq.com", userAddr, userSecurity);
+
+		transport.sendMessage(msg, new Address[] { new InternetAddress(email) });
+		transport.close();
+		return JSON.toJSONString("success");
+
+	}
+}
