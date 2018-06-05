be7586e685675b8475cfda85eb0db211ece61621
guanxin <m17621995911@163.com>
2018-06-05 10:05:17 +0800

    guan180605

---
+++ src/main/java/com/service/opinionService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+public interface opinionService {
+	/**
+	 * 查询建议
+	 * @return
+	 */
+	public String queryOpinionAll(String data);
+	
+	/**
+	 * 删除订单
+	 * @param data
+	 * @return
+	 */
+	public String deleteOpinionTab(String data);
+}
