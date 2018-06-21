227eb760d6f1f3c8c5f1af5bd8544630407fda46
smilehcb <605574504@qq.com>
2018-06-21 22:54:47 +0800

    6.21 hcb push before

--- src/main/webapp/martix/assets/js/common.js
+++ src/main/webapp/martix/assets/js/common.js
@@ -1,9 +1,9 @@
 /*!
  * 通用js文件
  * 1.加载前端是否显示的页面控制
- * 2. 
+ * 2.页面退出功能 
  */
- var url = "/videoServer/";
+var url = "/videoServer/";
  
 function loadRoleInfo(){
 	$.ajax({
@@ -22,5 +22,22 @@ function loadRoleInfo(){
 				$(".role").css("display","block");
 			}
 		}
-});
+	});
+}
+
+function teacherLoginOut(){
+	$.ajax({
+		type: "POST",
+		url: url+"teacher/teacherLogout",
+		datatype:'json',
+		async:true,
+		cache: false,  //禁用缓存
+		data: {
+			"teacher_id":12
+		},  
+		dataType: "json",
+		success: function (data) {
+			window.location.href = url+'martix/login.html';  
+		}
+	});
 }
