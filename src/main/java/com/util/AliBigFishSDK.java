d0c03d8f36df95c8cf76dd9fbbec74071ec885d6
aaaGit <pange.yu@pactera.com>
2018-07-13 23:29:05 +0800

    ss

--- src/main/java/com/util/AliBigFishSDK.java
+++ src/main/java/com/util/AliBigFishSDK.java
@@ -15,6 +15,7 @@ import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 
+@SuppressWarnings("unchecked")
 public class AliBigFishSDK {
 	/**
 	 * 
@@ -27,8 +28,11 @@ public class AliBigFishSDK {
 	public static String SendSMSAli(String data) throws ClientException {
 		HashMap<String,Object> map = JSON.parseObject(data, HashMap.class);
 		String phone = map.get("phone").toString();
+		System.out.println(phone);
 		String code = map.get("code").toString();
+		System.out.println(code);
 		String TemplateCode = map.get("TemplateCode").toString();
+		System.out.println(TemplateCode);
 		//设置超时时间-可自行调整
 		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
 		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
