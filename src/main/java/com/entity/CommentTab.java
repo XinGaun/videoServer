46071eaa566351997e62d310942e6dfac28cb319
zhengfei <508828384@qq.com>
2018-09-12 21:35:16 +0800

    comment

--- src/main/java/com/entity/CommentTab.java
+++ src/main/java/com/entity/CommentTab.java
@@ -11,33 +11,7 @@ public class CommentTab{
 	String replyText ;
 	int replyTeacher ;
 	Date replyDate ;
-	
-	public CommentTab() {
-	
-	}
-	
-	/*public CommentTab(HashMap<String,Object> demo) {
-		if(demo.containsKey("commentId")) {
-			this.commentId = (int) demo.get("commentId");
-		}
-		if(demo.containsKey("userId")) {
-			this.userId = (int) demo.get("userId");
-		}
-		if(demo.containsKey("commentText")) {
-			this.commentText = (String) demo.get("commentText");
-		}
-		if(demo.containsKey("commentDate")) {
-			this.commentDate =new Date();
-		}
-		if(demo.containsKey("replyText")) {
-			this.replyText =  demo.get("replyText").toString();
-		}
-		if(demo.containsKey("replyTeacher")) {
-			this.replyTeacher =(int)demo.get("replyTeacher");
-		}
-		
-	}*/
-	
+	int comment_status;
 	public int getCommentId() {
 		return commentId;
 	}
@@ -86,4 +60,11 @@ public class CommentTab{
 	public void setReplyDate(Date replyDate) {
 		this.replyDate = replyDate;
 	}
+	public int getComment_status() {
+		return comment_status;
+	}
+	public void setComment_status(int comment_status) {
+		this.comment_status = comment_status;
+	}
+	
 }
